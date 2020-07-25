307a0f024faac2c01d89a03a63a43003cd563005
Ryan Jansen <rjgiga444@gmail.com>
2020-07-25 13:34:20 +0800

    Finished Widget

--- widget/src/App.js
+++ widget/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -36,7 +37,7 @@ const options = [
 export default () => {
   return (
     <div>
-      <Dropdown options={options} />
+      <Translate />
     </div>
   );
 };
