240b0719c13273576cb2a2cceecb7a3bead93ed1
Ryan Jansen <ryanjansen@2.8.126.129.static.m1net.com.sg>
2020-08-22 14:02:13 +0800

    Finished Blog Project

---
+++ blog/src/actions/index.js
@@ -0,0 +1,26 @@
+import JSONPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await JSONPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await JSONPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await JSONPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
