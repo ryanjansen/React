8836c82c728f1a39548743c742941446ae1710d5
Ryan Jansen <rjgiga444@gmail.com>
2020-08-26 21:40:44 +0800

    Updated Streaming App

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import StreamList from "./streams/StreamList";
 import StreamDelete from "./streams/StreamDelete";
--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducers from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
